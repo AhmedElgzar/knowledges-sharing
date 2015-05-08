fe05d89b6c1ca8b80dac53683f1317a9894fdd26
ybigus <bigusyaroslav@gmail.com>
2015-05-08 15:12:01 +0300

    add rxnet paint example

---
+++ .net/RxNet/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace RxNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.canvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.canvas.Location = new System.Drawing.Point(14, 13);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(258, 237);
+            this.canvas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "PaintRx";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
---
+++ .net/RxNet/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RxNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var drawStart = Observable.FromEventPattern<MouseEventArgs>(canvas, "MouseDown");
+            var move = Observable.FromEventPattern<MouseEventArgs>(canvas, "MouseMove");
+            var drawEnd = Observable.FromEventPattern<MouseEventArgs>(canvas, "MouseUp");
+
+            int x = 0, y = 0;
+            var drawStream = drawStart.SelectMany(e =>
+            {
+                x = e.EventArgs.Location.X;
+                y = e.EventArgs.Location.Y;
+                return move.TakeUntil(drawEnd);
+            });
+
+            drawStream.Subscribe(evt =>
+            {
+                var newX = evt.EventArgs.Location.X;
+                var newY = evt.EventArgs.Location.Y;
+
+                var color = new Pen(Color.Black);
+                Graphics g = canvas.CreateGraphics();
+                g.DrawLine(color, new Point(x, y), new Point(newX, newY));
+
+                x = newX;
+                y = newY;
+            });
+        }
+    }
+}
